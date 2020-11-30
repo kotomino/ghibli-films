5ac5427488f7e82cdbae6aa41060b1b1ca0ac255
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2020-11-30 17:26:00 -0500

    initial commit

---
+++ lib/environment.rb
@@ -0,0 +1,7 @@
+require "pry"
+require "rest-client"
+require "json"
+
+require_relative "./models/pokemon"
+require_relative "./services/api"
+require_relative "./services/cli"
---
+++ lib/services/cli.rb
@@ -0,0 +1,7 @@
+class Cli
+
+    def start
+        puts "Welcome, Pokemon Trainer!"
+        puts "Loading..."
+    end
+end
